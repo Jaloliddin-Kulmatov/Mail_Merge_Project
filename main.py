4d20ac3a258d00875f1bbe26f0023cc0e0d4a026
jaloliddinqulmatov <jaloliddinqulmatov12@gmail.com>
2025-05-04 00:02:59 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+PLACEHOLDER = "[name]"
+with open("Input/Names/invited_names.txt", mode="r") as names_file:
+    names = names_file.readlines()
+
+with open("Input/Letters/starting_letter.txt", mode="r") as letter_file:
+    letter = letter_file.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter.replace(PLACEHOLDER, stripped_name)
+        with open(f"Output/ReadyToSend/letter_for_{stripped_name}.docs", mode="w") as completed_letter_file:
+            completed_letter_file.write(new_letter)
